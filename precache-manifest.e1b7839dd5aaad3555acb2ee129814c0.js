cddbe16ce9177d21d022185db7bead178b9d7306
Ashford <ruroland@126.com>
2020-03-09 22:28:38 +0800

    reset2

---
+++ precache-manifest.e1b7839dd5aaad3555acb2ee129814c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f78130dd1cbc7ed45012e535bf001d5",
+    "url": "/Page/index.html"
+  },
+  {
+    "revision": "b0d7ad36150768c2020b",
+    "url": "/Page/static/css/3.dfe80d2f.chunk.css"
+  },
+  {
+    "revision": "51a4473db710f3d5ffc8",
+    "url": "/Page/static/css/4.4841d763.chunk.css"
+  },
+  {
+    "revision": "7f7fc37d32fd9d0ddeb9",
+    "url": "/Page/static/css/5.4b1da974.chunk.css"
+  },
+  {
+    "revision": "a9ed70a75abb6b46640d",
+    "url": "/Page/static/css/main.75e5581a.chunk.css"
+  },
+  {
+    "revision": "37bdac1e34061cc37ae8",
+    "url": "/Page/static/js/2.fc2ceb85.chunk.js"
+  },
+  {
+    "revision": "b0d7ad36150768c2020b",
+    "url": "/Page/static/js/3.712d6c72.chunk.js"
+  },
+  {
+    "revision": "51a4473db710f3d5ffc8",
+    "url": "/Page/static/js/4.092fa352.chunk.js"
+  },
+  {
+    "revision": "7f7fc37d32fd9d0ddeb9",
+    "url": "/Page/static/js/5.0e6cb78e.chunk.js"
+  },
+  {
+    "revision": "5a6b8804ee83aaffef02",
+    "url": "/Page/static/js/6.e0d3c9cb.chunk.js"
+  },
+  {
+    "revision": "7ff8130f16924c69fdd4",
+    "url": "/Page/static/js/7.48ad3950.chunk.js"
+  },
+  {
+    "revision": "a9ed70a75abb6b46640d",
+    "url": "/Page/static/js/main.9630e3f8.chunk.js"
+  },
+  {
+    "revision": "67e4291466dfda311143",
+    "url": "/Page/static/js/runtime-main.56dc6984.js"
+  },
+  {
+    "revision": "55a57e9049d8167068c3eea4eef92154",
+    "url": "/Page/static/media/mountain.55a57e90.jpg"
+  }
+]);
